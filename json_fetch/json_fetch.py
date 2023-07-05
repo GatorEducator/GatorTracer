7c27559cf9ead1816c9a8a4e75b38a6bf0716217
Yanqiao4396 <chen02@allegheny.edu>
2023-07-05 10:19:24 -0400

    Doc:add type hint& missing docstring in json_fetch

--- json_fetch/json_fetch.py
+++ json_fetch/json_fetch.py
@@ -2,14 +2,13 @@ import base64
 import copy
 import json
 import re
-from typing import List, Tuple
 from pathlib import Path
-from config_console import Token
-from github import Github, Organization
-import pandas as pd
+from typing import Dict, List, Pattern, Tuple
 
-# dev
-import numpy as np
+import pandas as pd
+from config_console import Token
+from github import Github, Organization, Repository
+
 # TODO: add error msg and passing msg
 
 
@@ -24,7 +23,7 @@ class JsonFetch:
         ) = instructions
         self.out_dict = {"organizations": []}
 
-    def get_insight_jsons(self, dir, branch, file_regex):
+    def get_insight_jsons(self, dir: str, branch: str, file_regex: Pattern[str]):
         """Find all the matching repos and orgs."""
         self.dir, self.branch, self.file_regex = dir, branch, file_regex
         self.find_matching_orgs()
@@ -59,9 +58,8 @@ class JsonFetch:
                             "repositories": self.find_matching_repos(org),
                         }
                     )
-        return True
 
-    def find_matching_repos(self, org_obj):
+    def find_matching_repos(self, org_obj: Organization.Organization) -> List[Dict]:
         """Find all the matching repositories in a organization based on the inclusion/exclusion instruction and put them into dictionary."""
         # Combine a list of regular expressions with OR gate
         # If actual expression matches with any of expected regular expressions, then check should pass
@@ -92,7 +90,7 @@ class JsonFetch:
                     )
         return matching_repos
 
-    def find_matching_files(self, repo_obj):
+    def find_matching_files(self, repo_obj: Repository.Repository) -> List[Dict]:
         """Fetch all the immediate json files in a directory."""
         files_dict = []
         for f in repo_obj.get_contents(self.dir, ref=self.branch):
@@ -104,7 +102,9 @@ class JsonFetch:
                 decoded_content = base64.b64decode(f.content).decode("utf-8")
                 # get file name without extension
                 f_pure_name = ".".join(f.name.split(".")[:-1])
-                files_dict.append({"file-name": f_pure_name, "insight": decoded_content})
+                files_dict.append(
+                    {"file-name": f_pure_name, "insight": decoded_content}
+                )
         return files_dict
 
     def fetch_latest_json(self, dir):
@@ -117,7 +117,7 @@ class JsonFetch:
 class TreeDict:
     """A nested dictionary."""
 
-    def __init__(self, nested_dict) -> None:
+    def __init__(self, nested_dict: Dict) -> None:
         """Create TreeDict instance with a dictionary"""
         self.__nested_dict = nested_dict
 
@@ -125,21 +125,28 @@ class TreeDict:
         """return the nested_dictionary"""
         return str(self.__nested_dict)
 
-    def to_flatten_matrix(self, parsing_insight:bool = False):
+    def to_flatten_matrix(self, parsing_insight: bool) -> List[List]:
+        """Convert nested dictionary to matrix."""
         file_lvl_matrix = self.flatten_matrix_without_parsing_insight_json()
         if not parsing_insight:
             return file_lvl_matrix
         else:
-            self.matrix_with_flatten_insight_json(file_lvl_matrix)
+            file_lvl_matrix_w_parsed_insight = self.matrix_with_flatten_insight_json(
+                file_lvl_matrix
+            )
+            return file_lvl_matrix_w_parsed_insight
 
-    def matrix_with_flatten_insight_json(self, matrix_with_header):
+    def matrix_with_flatten_insight_json(
+        self, matrix_with_header: List[List]
+    ) -> List[List]:
+        """Call function to get parsed nested list insights and put them into the matrix."""
         main_df = pd.DataFrame(matrix_with_header[1:], columns=matrix_with_header[0])
         insights = main_df["insight"]
         print(insights)
         del main_df["insight"]
         for insight_idx in range(len(insights)):
             insight_str = insights[insight_idx]
-            # load the content of insight in json format  
+            # load the content of insight in json format
             insight_dict = json.loads(insight_str)
             insight_w_header = InsightReport(insight_dict).to_array_with_header()
             # Get all checks state
@@ -148,20 +155,20 @@ class TreeDict:
 
             # Find matching headers in the main df
             for i in range(len(insight_headers)):
-                insight_header =insight_headers[i]
+                insight_header = insight_headers[i]
                 # If fails to find one then create an empty one column with the insight header
                 if insight_header not in main_df.columns:
                     main_df[insight_header] = None
 
-                main_df.loc[insight_idx,insight_header] = insight_values[i]
-        
-        main_df.to_csv("report.csv")
+                main_df.loc[insight_idx, insight_header] = insight_values[i]
+        # nested list without header
+        matrix_w_parsed_insight = main_df.values.tolist()
+        header = list(main_df.columns)
+        # insert header to the first row
+        matrix_w_parsed_insight.insert(0, header)
+        return matrix_w_parsed_insight
 
-        
-
-
-
-    def flatten_matrix_without_parsing_insight_json(self) -> List:
+    def flatten_matrix_without_parsing_insight_json(self) -> List[List]:
         """Flatten the nested dictionary based on the leaf and put into matrix."""
         rows = []
         title = []
@@ -199,19 +206,25 @@ class TreeDict:
 
 
 class InsightReport:
-    def __init__(self, insight_dict) -> None:
+    def __init__(self, insight_dict: Dict) -> None:
         self.insight_dict = insight_dict
-    
-    def to_array_with_header(self):
-        array_w_header = [[],[]]
-        
+
+    def to_array_with_header(self) -> List[List]:
+        array_w_header = [[], []]
+
         # Add percentage_score and amount_correct
 
-        array_w_header[0].extend(["passing_percentage","amount_correct"])
+        array_w_header[0].extend(["passing_percentage", "amount_correct"])
         # Convert a percentage score string to a decimal percentage ex: "53" to 0.53
-        passing_percentage = "{0:.2f}".format(int(self.insight_dict["percentage_score"])*0.01)
-        array_w_header[1].append(passing_percentage) if "percentage_score" in self.insight_dict else array_w_header[1].append(None)
-        array_w_header[1].append(int(self.insight_dict["amount_correct"])) if "amount_correct" in self.insight_dict else array_w_header[1].append(None)
+        passing_percentage = "{0:.2f}".format(
+            int(self.insight_dict["percentage_score"]) * 0.01
+        )
+        array_w_header[1].append(
+            passing_percentage
+        ) if "percentage_score" in self.insight_dict else array_w_header[1].append(None)
+        array_w_header[1].append(
+            int(self.insight_dict["amount_correct"])
+        ) if "amount_correct" in self.insight_dict else array_w_header[1].append(None)
 
         checks = dict()
         for check in self.insight_dict["checks"]:
@@ -226,12 +239,12 @@ class InsightReport:
 
             # distinguish the checks with different fragments. e.x.: MatchFileFragment_TODO isn't the same with MatchFileFragment_NAME
             if "options" in check and "fragment" in check["options"]:
-                check_type +=  "_" + str(check["options"]["fragment"])
-            
+                check_type += "_" + str(check["options"]["fragment"])
+
             # implement AND logic. If multi checks have the same check type. Pass True iff all the status is True
             if check_type not in checks:
                 checks[check_type] = check["status"]
-            
+
             # as long as one of the checks with the same type is False, then the check type should be False
             elif checks[check_type] == True and check["status"] == False:
                 checks[check_type] = False
@@ -250,7 +263,7 @@ class InsightReport:
 
 
 if __name__ == "__main__":
-    with open("json_fetch/config/out.json","r") as js:
+    with open("json_fetch/config/out.json", "r") as js:
         json_report = json.load(js)
     print(type(json_report))
     insight_report = TreeDict(json_report)
