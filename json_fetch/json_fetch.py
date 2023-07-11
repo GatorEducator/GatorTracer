2dd91c6360ea35828af8e781f37dfaba608f8ff6
Yanqiao4396 <chen02@allegheny.edu>
2023-07-11 16:35:16 -0400

    lint: json_fetch

--- json_fetch/json_fetch.py
+++ json_fetch/json_fetch.py
@@ -1,15 +1,16 @@
+"""Fetch jsons from GitHub."""
 import base64
 import copy
-import json
 import re
 from typing import Dict, List, Pattern, Tuple
 
 from github import Github, Organization, Repository
 
-# TODO: add error msg and passing msg
-
-
 class JsonFetch:
+    """Fetch Json."""
+
+    # pylint: disable=too-many-instance-attributes
+    # pylint: disable=attribute-defined-outside-init
     def __init__(self, token: str, instructions: Tuple) -> None:
         self.authenticated_api = Github(token)
         (
@@ -20,24 +21,25 @@ class JsonFetch:
         ) = instructions
         self.out_dict = {"organizations": []}
 
-    def get_insight_jsons(self, dir: str, branch: str, file_regex: Pattern[str]):
+    def get_insight_jsons(self, directory: str, branch: str, file_regex: Pattern[str]):
         """Find all the matching repos and orgs."""
-        self.dir, self.branch, self.file_regex = dir, branch, file_regex
+        self.directory, self.branch, self.file_regex = directory, branch, file_regex
         self.find_matching_orgs()
         return TreeDict(self.out_dict)
 
     def find_matching_orgs(self):
-        """Find all the matching organizations based on the inclusion/exclusion instruction and call finding_matching_repos."""
+        """Find all the matching organizations based on the inclusion/exclusion instruction
+        and call finding_matching_repos."""
         # Combine a list of regular expressions with OR gate
-        # If actual expression matches with any of expected regular expressions, then check should pass
+        # If actual expression matches with any of expected regular expressions, check should pass
         included_combined = "(" + ")|(".join(self.included_orgs) + ")"
         excluded_combined = "(" + ")|(".join(self.excluded_orgs) + ")"
 
         # If included orgs are specified, then only fetch the orgs matching with included orgs
         # Otherwise fetch all the orgs not matching with the excluded orgs
         if self.included_orgs:
-            # DOCUMENTATION: use organization login other than name (i.e. use the url org name)
-            # DOCUMENTATION: students have to be at least member to make the organization listed here, remind faculty to do so
+            # use organization login other than name (i.e. use the url org name)
+            # students have to be at least member in the organization
             for org in self.authenticated_api.get_user().get_orgs():
                 if org.login and re.match(included_combined, org.login):
                     self.out_dict["organizations"].append(
@@ -57,9 +59,10 @@ class JsonFetch:
                     )
 
     def find_matching_repos(self, org_obj: Organization.Organization) -> List[Dict]:
-        """Find all the matching repositories in a organization based on the inclusion/exclusion instruction and put them into dictionary."""
+        """Find all the matching repositories in a organization
+        based on the inclusion/exclusion instruction and put them into dictionary."""
         # Combine a list of regular expressions with OR gate
-        # If actual expression matches with any of expected regular expressions, then check should pass
+        # If actual expression matches with any of expected regular expressions, check should pass
         included_combined = "(" + ")|(".join(self.included_repos) + ")"
         excluded_combined = "(" + ")|(".join(self.excluded_repos) + ")"
 
@@ -88,9 +91,9 @@ class JsonFetch:
         return matching_repos
 
     def find_matching_files(self, repo_obj: Repository.Repository) -> List[Dict]:
-        """Fetch all the immediate json files in a directory."""
+        """Fetch all the immediate json files in a directoryectory."""
         files_dict = []
-        for f in repo_obj.get_contents(self.dir, ref=self.branch):
+        for f in repo_obj.get_contents(self.directory, ref=self.branch): # pylint: disable = invalid-name
             if (
                 f.type == "file"
                 and f.name.endswith(".json")
@@ -109,7 +112,7 @@ class TreeDict:
     """A nested dictionary."""
 
     def __init__(self, nested_dict: Dict) -> None:
-        """Create TreeDict instance with a dictionary"""
+        """Create TreeDict instance with a dictionary."""
         self.__nested_dict = nested_dict
 
     def __str__(self):
@@ -122,16 +125,23 @@ class TreeDict:
         rows = []
         title = []
 
-        def flatten(d, values: List = []):
+        def flatten(root_dict, values: List = []):
+            """Recursively find the non-iterable and put into matrix.
+
+            Args:
+                d: dictionary
+                values: a list of recorded non-iterable values inherited from parent dictionary
+            """
             # The keys whose value is a list of dictionary
             keys_to_list = []
             found_list = False
-            for k in d:
-                if isinstance(d[k], str) or isinstance(d[k], int):
-                    values.append(d[k])
-                    title.append(k) if k not in title else None
-                elif isinstance(d[k], list) and all(
-                    isinstance(sub, dict) for sub in d[k]
+            for k in root_dict:
+                if isinstance(root_dict[k], (str,int)):
+                    values.append(root_dict[k])
+                    if k not in title:
+                        title.append(k)
+                elif isinstance(root_dict[k], list) and all(
+                    isinstance(sub, dict) for sub in root_dict[k]
                 ):
                     keys_to_list.append(k)
                     found_list = True
@@ -139,25 +149,18 @@ class TreeDict:
                     pass  # currently assume there is no other types
             # Base case: there is no more sub-dictionary
             if not found_list:
-                v = copy.deepcopy(values)
-                # Put all the static values like string, integer (non list nor dict) of ancestors (include the current node) into the rows
-                rows.append(v)
+                value = copy.deepcopy(values)
+                # Put all the non-iterable values (e.g.: int, str) into the rows
+                rows.append(value)
             # General case: call flatten function recursively with sub-dictionary as root
             else:
                 for k in keys_to_list:
-                    for sub_d in d[k]:
-                        v = copy.deepcopy(values)
-                        flatten(sub_d, v)
+                    for sub_d in root_dict[k]:
+                        value = copy.deepcopy(values)
+                        flatten(sub_d, value)
 
         flatten(self.__nested_dict)
         matrix_with_title = [title] + rows
         print("⭐ flat matrix was built successfully")
         return matrix_with_title
-
-
-if __name__ == "__main__":
-    with open("json_fetch/config/out.json", "r") as js:
-        json_report = json.load(js)
-    print(type(json_report))
-    insight_report = TreeDict(json_report)
-    insight_report.to_flatten_matrix(parsing_insight=True)
+        
